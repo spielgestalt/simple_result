f2eab04d1b03d7b6ed44796e084c21c3b6f5b2c1
Carsten Fregin <fregin@me.com>
2020-02-17 02:28:14 +0100

    adds documentation to the result class and pins version to 0.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
+## 0.1.0
+* adds documentation and formats code with dartfmt. 
 ## 0.0.2
 * changes Result<Failure,Success> to Result<Success,Failure>
 ## 0.0.1
--- lib/src/result.dart
+++ lib/src/result.dart
@@ -1,5 +1,7 @@
 import 'package:meta/meta.dart';
-
+/// A simple Result wrapper.
+///
+/// You can declare Success and Failure Types as Generic Type..
 class Result<Success, Failure> {
   final bool _isSuccess;
   final Success _value;
@@ -10,14 +12,18 @@ class Result<Success, Failure> {
         _value = value,
         _failure = failure;
 
+  /// Marks this Result as successful.
   factory Result.success(Success value) {
     return Result._(isSuccess: true, value: value, failure: null);
   }
 
+  /// Marks this Result as failing
   factory Result.failure(Failure failure) {
     return Result._(isSuccess: false, value: null, failure: failure);
   }
 
+  /// Somehow like a switch case statement. You can return a Value from the
+  /// functions success and failure.
   R when<R>(
       {@required R Function(Success) success,
       @required R Function(Failure) failure}) {
